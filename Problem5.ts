c406bbad620ce6e0f84d1b986918c2f3ba655954
AKsamrat <samratuap52@gmail.com>
2024-11-06 23:34:13 +0600

    problem5 solved

---
+++ Problem5.ts
@@ -0,0 +1,11 @@
+{
+  //
+
+  const getProperty = <s, n extends keyof s>(obj: s, name: n) => {
+    return obj[name];
+  };
+  // const person = { name: 'Alice', age: 30 };
+  // console.log(getProperty(person, 'age'));
+
+  //
+}
