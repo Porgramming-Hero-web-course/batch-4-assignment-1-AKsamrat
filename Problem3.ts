93f827ec413cc20d4b409e9d090a20a5e3a19ae8
AKsamrat <samratuap52@gmail.com>
2024-11-07 06:10:57 +0600

    problem 7 solved

--- Problem3.ts
+++ Problem3.ts
@@ -2,14 +2,20 @@
   //
 
   function countWordOccurrences(sentance: string, word: string): number {
-    let splitSentence: string[] = sentance.toLowerCase().split(' ');
+    let splitSentence: string[] = sentance
+      .toLowerCase()
+      .replace(/[.!*+?^${}()|[\]\\]/g, ' ')
+      .split(' ');
     const result: string[] = splitSentence.filter(
       item => item === word.toLowerCase()
     );
     return result.length;
   }
-  // console.log(
-  //   countWordOccurrences('TypeScript is great. I love TypeScript', 'Typescript')
-  // );
+  console.log(
+    countWordOccurrences(
+      'TypeScript is great. I love TypeScript!',
+      'Typescript'
+    )
+  );
   //
 }
