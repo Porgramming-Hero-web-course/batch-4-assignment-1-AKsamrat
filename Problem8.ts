a2a588e5830f6eba4095ca520fc1cc60eaae9ccf
AKsamrat <samratuap52@gmail.com>
2024-11-07 17:21:34 +0600

    callback add in blog

--- Problem8.ts
+++ Problem8.ts
@@ -1,12 +1,18 @@
 {
   //
-  function validateKeys<T>(obj: T, oKeys: (keyof T)[]): boolean {
-    return oKeys.every(key => Object.hasOwnProperty.call(obj, key));
+  type User = {
+    name: string;
+    age: number;
+    email: string;
+  };
+
+  function validateKeys<T extends User>(obj: T, oKeys: (keyof T)[]): boolean {
+    return oKeys.every(key => obj.hasOwnProperty(key));
     // const haskey = oKeys.filter(key => obj.hasOwnProperty(key));
     // if (haskey) return true;
     // else return false;
   }
-  // const person = { name: 'Alice', age: 25, email: 'alice@example.com' };
-  // console.log(validateKeys(person, ['name', 'email']));
+  const person = { name: 'Alice', age: 25, email: 'alice@example.com' };
+  console.log(validateKeys(person, ['name', 'email']));
   //
 }
---
+++ blog-1.md
@@ -0,0 +1,10 @@
+                            #CALLBAKCS
+
+A JavaScript callback is a function which is to be executed after another
+function has finished execution . Any function that is passed as an argument to
+another function so that it can be executed in that other function is called as
+a callback function. This results in callback hell.
+
+```tsx
+
+```
